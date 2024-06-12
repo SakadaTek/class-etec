ec9194c83c2a796563f178e9aa25b671f6df2845
SakadaTek <167770265+SakadaTek@users.noreply.github.com>
2024-06-12 18:38:04 +0700

    Add files via upload

---
+++ loop.js
@@ -0,0 +1,27 @@
+// Array
+let Array = [10,20,30];
+
+// for loop
+// for(i=0;i<Array.length;i++){
+//     console.log(Array[i]);
+// }
+
+//  while loop
+// i=0;
+// while(i<Array.length){
+//     console.log(Array[i]);
+//     i++;
+// }
+
+// do while loop
+// i=0;
+// do{
+//     console.log(Array[i]);
+// }while(i<Array.length);
+
+
+// var data;// create to store data
+// for(i in Array){
+//     data += console.log(Array[1]);
+// }
+Array.map(e)
---
+++ math.js
@@ -0,0 +1,30 @@
+// let a=92.99999012;
+// let b=25;
+// //ceil ,pow
+// const result = Math.pow(b,2);
+// console.log(result);
+// console.log(a.toFixed(2));
+
+// //spread opaerator in javasrcript
+// //1.combing Array
+// let A1 =[20,30,40];
+// let A2 =[30,440,70];
+// let A3 =[...A1,...A2];
+// console. log(A3);
+// //2.passing Argumwut
+// function num(x,y)
+// {
+//     return x*y;
+// }
+// let A4 = [20,30];
+// console.log(num(...A4));
+
+// //3. add Array
+// let Array2 =[0,...A4];
+// console.log(Array2);
+
+let a=50.47;
+let result =Math.floor(a);
+var id =Math.floor(Math.random()*100+1);
+//console.log(id.toFixed(0));
+console.log(id);
